44ff516b87f74efa50bf1a6faaf4ae791276e453
b1nj <b.simon@pixellweb.com>
2016-01-06 13:22:12 -0400

    Add TweetDeck Clear

---
+++ README.md
@@ -0,0 +1,13 @@
+# User Scripts
+
+This is a collection of user scripts I wrote. They're tested with Greasemonkey for Firefox.
+
+You can also find these scripts in my personal script sections on Greasy Fork and OpenUserJS.
+
+## Summary
+
+### TweetDeck Clear
+
+Add buttons to tweetdeck for clear all tweets or column tweets
+
+[Install](https://raw.githubusercontent.com/b1nj/userscripts/master/TweetDeck_Clear.user.js)
---
+++ TweetDeck_Clear.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name    TweetDeck Clear
+// @namespace   http://b1nj.fr
+// @version 0.2
+// @description Add buttons to tweetdeck for clear all tweets or column tweets
+// @match   https://tweetdeck.twitter.com/*
+// @match   http://tweetdeck.twitter.com/*
+// @match   https://www.tweetdeck.twitter.com/*
+// @match   http://www.tweetdeck.twitter.com/*
+// @grant       none
+// @copyright   Benjamin Simon
+// @license GPL version 3 or any later version; www.gnu.org/copyleft/gpl.htm
+// ==/UserScript==
+
+$(function() {
+
+    window.setTimeout(addButtons, 5000);
+
+    var button = '<button style="position: absolute; right: 10px; top: -10px; padding-bottom: 0px;" type="button" class="js-user-button btn btn-alt btn-neutral btn-options-tray padding-hn "> <i class="icon icon-clear-timeline"></i> <span class="label">Clear</span> </button>';
+    var buttonAll = '<a type="button" class="js-user-buttonAll padding-hl app-nav-link cf"><div class="obj-left"> <i class="icon icon-clear-timeline"></i> </div> <div class="nbfc padding-ts hide-condensed">Clear all</div> </a>';
+
+    function addButtons() {
+
+        // Buttons clear
+        $('.js-column-header').parent().each(function() {
+            $(this).append(button);
+        });
+        $('.js-user-button').click(function () {
+            var $col = $(this).parent();
+            $col.find('a[data-action="options"]').trigger('click');
+            window.setTimeout(function () {
+                clearTweets($col);
+            }, 1000);
+        });
+
+        // Button clear All
+        $('.app-navigator').append(buttonAll);
+        $('.js-user-buttonAll').click(function () {
+            $('.js-user-button').each(function() {
+                $(this).trigger('click');
+            });
+        });
+    }
+
+
+    function clearTweets($col) {
+        $('button[data-action="clear"]').each(function (index) {
+            $(this).trigger('click');
+        });
+        $('a[data-action="options"]', $col).trigger('click');
+    }
+
+});
