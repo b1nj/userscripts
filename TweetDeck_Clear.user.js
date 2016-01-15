a4d931d840d6ce17a49859955619cbfc06acc6f6
b1nj <postmaster@b1nj.fr>
2016-01-14 20:10:24 -0400

    use mustache

--- TweetDeck_Clear.user.js
+++ TweetDeck_Clear.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name    TweetDeck Clear
 // @namespace   http://b1nj.fr
-// @version 0.6
+// @version 0.6.1
 // @icon https://ton.twimg.com/tweetdeck-web/web/assets/logos/favicon.3d5d8f1cbe.ico
 // @description Add buttons and keyboard shortcuts to tweetdeck for clear all tweets or column tweets
 // @match   https://tweetdeck.twitter.com/*
@@ -13,117 +13,98 @@
 // @license License:X11 (MIT)
 // ==/UserScript==
 
-$(function() {
-
-    // Wait for the complete loading of the ajax page
-    // I have tried unsuccessfully other solutions with ajaxStop() for example.
-    window.setTimeout(load, 5000);
-
-    function load() {
-        addButtons();
-        addKeyboardShortcuts();
-        addTextKeyboardShortcuts();
-    }
-
-
+     
+function addButtons() {
     var button = '<button style="position: absolute; right: 10px; top: -10px; padding-bottom: 0px;" type="button" class="js-user-button btn btn-alt btn-neutral btn-options-tray padding-hn "> <i class="icon icon-clear-timeline"></i> <span class="label">Clear</span> </button>';
+    window.TD_mustaches["column/column_header.mustache"] = window.TD_mustaches["column/column_header.mustache"].replace(' </header>', button + ' </header>');
+
+    $(document).on('click', '.js-user-button', function () {
+        clearTweets($(this).parents('.js-column'));
+    });
+}
+
+function addButtonAll() {
     var buttonAll = '<a type="button" class="js-user-buttonAll padding-hl app-nav-link cf"><div class="obj-left"> <i class="icon icon-clear-timeline"></i> </div> <div class="nbfc padding-ts hide-condensed">Clear all</div> </a>';
+    window.TD_mustaches["topbar/app_header.mustache"] = window.TD_mustaches["topbar/app_header.mustache"].replace('</a> </nav>','</a> ' + buttonAll + ' </nav>');
 
-    function addButtons() {
-
-        // Buttons clear
-        $('.js-column-header').parent().each(function() {
-            $(this).append(button);
-        });
-        $('.js-user-button').click(function () {
-            var $col = $(this).parent();
-            $col.find('a[data-action="options"]').trigger('click');
-            window.setTimeout(function () {
-                clearTweets($col);
-            }, 1000);
-        });
-
-        // Button clear All
-        $('.app-navigator').append(buttonAll);
-        $('.js-user-buttonAll').click(function () {
-            $('.js-user-button').each(function() {
-                $(this).trigger('click');
-            });
-        });
-    }
-
-
-    function clearTweets($col) {
-        $('button[data-action="clear"]').each(function (index) {
+    $(document).on('click', '.js-user-buttonAll', function () {
+        $('.js-user-button').each(function() {
             $(this).trigger('click');
         });
-        $('a[data-action="options"]', $col).trigger('click');
-    }
+    });
+}
+
+function clearTweets($col) {
+    $buttonOptions = $('a[data-action="options"]', $col);
+    $buttonOptions.trigger('click');
+    window.setTimeout(function ($col, $buttonOptions) {
+        $('button[data-action="clear"]', $col).trigger('click');
+        $buttonOptions.trigger('click');
+    }, 1000, $col, $buttonOptions);    
+} 
 
 
-    // Add Keyboard shortcuts
-    var key = [];
-    function addKeyboardShortcuts() {
-        $(document).on("keydown keyup", function(e){
-            key[e.keyCode] = e.type == 'keydown';
+// Add Keyboard shortcuts
+var key = [];
+function addKeyboardShortcuts() {
+    $(document).on("keydown keyup", function(e){
+        key[e.keyCode] = e.type == 'keydown';
 
-            if (test_key('del')) {
-                $jsColumnFocused = $('.js-column.is-focused');
-                if ($jsColumnFocused.length) {
-                    $jsColumnFocused.find('.js-user-button').trigger('click');
-                    key = [];
-                }
-
-                $isSelectedTweet = $('.is-selected-tweet');
-                if ($isSelectedTweet.length) {
-                    $isSelectedTweet.parents('.js-column').find('.js-user-button').trigger('click');
-                    key = [];
-                }
-            }
-
-            if (test_keys('alt', 'del')) {
-                $('.js-user-buttonAll').trigger('click');
+        if (test_key('del')) {
+            $jsColumnFocused = $('.js-column.is-focused');
+            if ($jsColumnFocused.length) {
+                $jsColumnFocused.find('.js-user-button').trigger('click');
                 key = [];
             }
-        });
-    }
 
-    /* http://stackoverflow.com/questions/5203407/javascript-multiple-keys-pressed-at-once */
-    function test_key(selkey){
-        var alias = {
-            "alt":   18,
-            "ctrl":  17,
-            "shift": 16,
-            "C":     67,
-            "del":   46,
-        };
-        return key[selkey] || key[alias[selkey]];
-    }
-    function test_keys(){
-        var i,
-            keylist = arguments,
-            status = true;
-
-        for(i = 0; i < keylist.length; i++){
-            if(!test_key(keylist[i])){
-                status = false;
+            $isSelectedTweet = $('.is-selected-tweet');
+            if ($isSelectedTweet.length) {
+                $isSelectedTweet.parents('.js-column').find('.js-user-button').trigger('click');
+                key = [];
             }
         }
-        return status;
+
+        if (test_keys('alt', 'del')) {
+            $('.js-user-buttonAll').trigger('click');
+            key = [];
+        }
+    });
+}
+
+/* http://stackoverflow.com/questions/5203407/javascript-multiple-keys-pressed-at-once */
+function test_key(selkey){
+    var alias = {
+        "alt":   18,
+        "ctrl":  17,
+        "shift": 16,
+        "C":     67,
+        "del":   46,
+    };
+    return key[selkey] || key[alias[selkey]];
+}
+function test_keys(){
+    var i,
+        keylist = arguments,
+        status = true;
+
+    for(i = 0; i < keylist.length; i++){
+        if(!test_key(keylist[i])){
+            status = false;
+        }
     }
+    return status;
+}
 
-
+function addInfosShortcuts(){
     // Add text Keyboard shortcuts in modal
     var txt  = '<dd class="keyboard-shortcut-definition"><span class="text-like-keyboard-key">Del</span> Clear active column</dd>'
              + '<dd id="js-keyboard-clear" class="keyboard-shortcut-definition"><span class="text-like-keyboard-key">1</span>…<span class="text-like-keyboard-key">9</span> + <span class="text-like-keyboard-key">Del</span>  Clear column 1－9</dd>'
              + '<dd class="keyboard-shortcut-definition"><span class="text-like-keyboard-key">Alt</span> + <span class="text-like-keyboard-key">Del</span> Clear all</dd>';
 
-    function addTextKeyboardShortcuts() {
-       $('#open-modal').on("DOMSubtreeModified", function(){
-           if (!$('#js-keyboard-clear').length) {
-               $('.keyboard-shortcut-list').first().append(txt);
-           }
-       });
-    }
+    window.TD_mustaches["keyboard_shortcut_list.mustache"] = window.TD_mustaches["keyboard_shortcut_list.mustache"].replace('</dl>  <dl', ' ' + txt + ' </dl>  <dl');
+}
 
-});
+addButtons();
+addButtonAll();
+addKeyboardShortcuts();
+addInfosShortcuts();
